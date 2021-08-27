03a628de3ceb9553cdd1b7fd55c2372ff6f3fc76
baso <bartlomiej.sopecki@mobica.com>
2021-08-27 17:08:42 +0200

    linked list sorting implementation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NUmbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LInkedList';
 
 const numbersCollection = new NUmbersCollection([1, 2, 45, 67, 98, 54, -5, 67]);
 const sorter = new Sorter(numbersCollection);
@@ -13,3 +14,13 @@ const charactersCollection = new CharactersCollection(
 const charsSorter = new Sorter(charactersCollection);
 charsSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(12);
+linkedList.add(-40);
+linkedList.add(68);
+
+const listSorter = new Sorter(linkedList);
+listSorter.sort();
+linkedList.print();
